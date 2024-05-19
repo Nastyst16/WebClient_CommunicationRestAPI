06c6527e66a13f19adc1b87fb2ed4bc2c31df88b
Nastyst16 <cristian_nastase16@yahoo.com>
2024-05-20 00:02:02 +0300

    working for get_books command

--- client.c
+++ client.c
@@ -43,9 +43,27 @@ typedef struct {
     char response[BUFLEN];
     char cookies[BUFLEN];
     char *request;
-    char *tokens[BUFLEN];
+    char tokens[BUFLEN];
 
 } t_client;
+
+int verify_error(t_client *client) {
+
+    int ok_error = 0;
+
+    if (strstr(client->response, "error") != NULL) {
+
+        char *error = strstr(client->response, "error");
+        error += 8;
+        error[strlen(error) - 2] = '\0'; // remove the last "}
+
+        printf("%s\n", error);
+
+        ok_error = 1;
+    }
+
+    return ok_error;
+}
 
 int parse_command(char *argv[])
 {
@@ -98,7 +116,7 @@ void register_command(t_client client) // done
     strcpy(client.message, json_serialize_to_string_pretty(root_value));
 
     char *data[] = {client.message};
-    client.request = compute_post_request(HOST, "/api/v1/tema/auth/register", "application/json", data, 1, NULL, 0);
+    client.request = compute_post_request(HOST, "/api/v1/tema/auth/register", "application/json", data, 1, NULL, 0, NULL);
 
     send_to_server(sockfd, client.request);
 
@@ -142,24 +160,20 @@ void login_command(t_client *client)
     strcpy(client->message, json_serialize_to_string_pretty(root_value));
 
     char *data[] = {client->message};
-    client->request = compute_post_request(HOST, "/api/v1/tema/auth/login", "application/json", data, 1, NULL, 0);
+    client->request = compute_post_request(HOST, "/api/v1/tema/auth/login", "application/json", data, 1, NULL, 0, NULL);
 
     send_to_server(sockfd, client->request);
 
     strcpy(client->response, receive_from_server(sockfd));
 
     // searching for the ERROR string in the response
-    if (strstr(client->response, "error") != NULL) {
-
-        char *error = strstr(client->response, "error");
-        error += 8;
-        error[strlen(error) - 2] = '\0'; // remove the last "}
-
-        printf("%s\n", error);
-
+    if (!verify_error(client)) {
+        printf("Success, user: %s logged in.\n", client->data0);
     } else {
-        printf("Success, user: %s registered.\n", client->data0);
+        return;
     }
+
+
 
     // no error. save the cookie
     char *cookie = strstr(client->response, "Set-Cookie: ");
@@ -194,28 +208,130 @@ void enter_library_command(t_client *client) {
     strcpy(client->response, receive_from_server(sockfd));
 
     // searching for the ERROR string in the response
-    if (strstr(client->response, "error") != NULL) {
-
-        char *error = strstr(client->response, "error");
-        error += 8;
-        error[strlen(error) - 2] = '\0'; // remove the last "}
-
-        printf("%s\n", error);
-
+    if (!verify_error(client)) {
+        printf("Success, user: %s entered the library.\n", client->data0);
     } else {
-        // printf("Success, user: %s entered the library.\n", client->data0);
-        printf("%s", client->response);
+        return;
     }
+
 
     // saving the token
     char *token = strstr(client->response, "token");
     token += 8;
     token[strlen(token) - 2] = '\0';
 
+    memset(client->tokens, 0, BUFLEN);
     strcpy(client->tokens, token);
-
-    debug(client->tokens, -1);
 }
+
+
+void get_books_command(t_client *client) {
+
+    int sockfd = client->sockfd;
+
+    if (strlen(client->tokens) == 0) {
+        printf("You must be logged in to enter the library.\n");
+        return;
+    }
+
+    // create the message
+    client->request = compute_get_request(HOST, "/api/v1/tema/library/books", NULL, NULL, 0, client->tokens);
+
+    send_to_server(sockfd, client->request);
+
+    strcpy(client->response, receive_from_server(sockfd));
+
+    // searching for the ERROR string in the response
+    if (!verify_error(client)) {
+
+        JSON_Value *root_value = json_parse_string(client->response);
+        JSON_Array *books = json_value_get_array(root_value);
+
+        for (int i = 0; i < json_array_get_count(books); i++) {
+            JSON_Object *book = json_array_get_object(books, i);
+
+            printf("title: %s\n", json_object_get_string(book, "title"));
+            printf("author: %s\n", json_object_get_string(book, "author"));
+            printf("genre: %s\n", json_object_get_string(book, "genre"));
+            printf("publisher: %s\n", json_object_get_string(book, "publisher"));
+            printf("page_count: %d\n", (int)json_object_get_number(book, "page_count"));
+            printf("\n");
+        }
+
+
+    } else {
+        return;
+    }
+}
+
+
+void add_book_command(t_client *client) {
+
+    int sockfd = client->sockfd;
+
+    if (strlen(client->tokens) == 0) {
+        printf("You must be logged in to enter the library.\n");
+        return;
+    }
+    // verify if token exists
+    if (strlen(client->tokens) == 0) {
+        printf("You must be logged in to enter the library.\n");
+        return;
+    }
+
+
+    // create the message
+    JSON_Value *root_value = json_value_init_object();
+    JSON_Object *root_object = json_value_get_object(root_value);
+
+    char book[BUFLEN];
+    fgets(book, BUFLEN, stdin);
+
+    printf("title=");
+    memset(book, 0, BUFLEN);
+    fgets(book, BUFLEN, stdin);
+    json_object_set_string(root_object, "title", book);
+
+    printf("author=");
+    memset(book, 0, BUFLEN);
+    fgets(book, BUFLEN, stdin);
+    json_object_set_string(root_object, "author", book);
+
+    printf("genre=");
+    memset(book, 0, BUFLEN);
+    fgets(book, BUFLEN, stdin);
+    json_object_set_string(root_object, "genre", book);
+
+    printf("publisher=");
+    memset(book, 0, BUFLEN);
+    fgets(book, BUFLEN, stdin);
+    json_object_set_string(root_object, "publisher", book);
+
+    printf("page_count=");
+    memset(book, 0, BUFLEN);
+    fgets(book, BUFLEN, stdin);
+    json_object_set_number(root_object, "page_count", atoi(book));
+
+
+    strcpy(client->message, json_serialize_to_string_pretty(root_value));
+
+    char *data[] = {client->message};
+    client->request = compute_post_request(HOST, "/api/v1/tema/library/books", "application/json",
+                                            data, 1, NULL, 0, client->tokens);
+
+    send_to_server(sockfd, client->request);
+
+    strcpy(client->response, receive_from_server(sockfd));
+
+    // searching for the ERROR string in the response
+    if (!verify_error(client)) {
+        printf("Success, user: %s added a book.\n", client->data0);
+    } else {
+        printf("%s", client->response);
+        return;
+    }
+}
+
 
 
 
@@ -253,7 +369,7 @@ int main(int argc, char *argv[])
                 break;
 
             case GET_BOOKS:
-
+                get_books_command(&client);
 
                 break;
 
@@ -262,6 +378,7 @@ int main(int argc, char *argv[])
                 break;
 
             case ADD_BOOK:
+                add_book_command(&client);
 
                 break;
 
--- requests.c
+++ requests.c
@@ -59,7 +59,7 @@ char *compute_get_request(char *host, char *url, char *query_params,
 }
 
 char *compute_post_request(char *host, char *url, char* content_type, char **body_data,
-                            int body_data_fields_count, char **cookies, int cookies_count)
+                            int body_data_fields_count, char **cookies, int cookies_count, char *token)
 {
     int i, len;
     char *message = calloc(BUFLEN, sizeof(char));
@@ -101,6 +101,11 @@ char *compute_post_request(char *host, char *url, char* content_type, char **bod
         }
 
         sprintf(line, "%s%s", line, cookies[cookies_count - 1]);
+        compute_message(message, line);
+    }
+
+    if(token != NULL){
+        sprintf(line, "Authorization: Bearer %s", token);
         compute_message(message, line);
     }
 
